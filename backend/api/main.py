cbc4aadf53eed3bc868bbf9580ce42dfc339d9b7
Stefannsen <stefan.negulescu@my.fmi.unibuc.ro>
2025-03-13 22:08:11 +0200

    Update main

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,3 +1,37 @@
-import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from .app_package.notes import get_notes
+from .app_package.quiz import get_quiz
 
-uvicorn.run("app-package.api:app", host="0.0.0.0", port=8000, reload=True)
+
+app = FastAPI()
+
+# origins = [
+#     "http://localhost:3000",
+#     "localhost:3000",
+#     "https://ai-learning-app-a673.vercel.app/",
+#     "ai-learning-app-a673.vercel.app/"
+# ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"data": 'Home'}
+
+@app.post("/notes")
+async def notes(request: Request) -> dict:
+    body = await request.json()
+    return get_notes(body['data']['text'])
+
+@app.post("/quiz")
+async def quiz(request: Request) -> dict:
+    body = await request.json()
+    return get_quiz(body['data']['text'])
